3b426b8a9bf95ed27e3ba4c22b06e8a822afad06
Daniel Keep <daniel.keep@gmail.com>
2015-08-02 00:47:42 +1000

    Added documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,89 @@
 /*!
-Provides a very bare-bones way of having custom `derive` support in stable Rust.
+This crate provides a macro that enables the use of custom `derive` attributes.
+
+To use it, make sure you link to the crate like so:
+
+```rust
+#[macro_use] extern crate custom_derive;
+# macro_rules! Dummy { (() struct $name:ident;) => {}; }
+# custom_derive! { #[derive(Clone, Dummy)] struct Foo; }
+# fn main() { let _ = Foo; }
+```
+
+> **Note**: the `custom_derive!` macro itself is not documented, as the automatic documentation for it would be uselessly huge and incomprehensible.
+
+# Usage
+
+The macro should be used to wrap an entire *single* `enum` or `struct` declaration, including its attributes (both `derive` and others).  All derivation attributes which the macro does *not* recognise will be assumed to be custom, and treated accordingly.
+
+`custom_derive!` assumes that custom derivations are implemented as macros (of the same name).  For example, here is a simple derivation macro:
+
+```rust
+#[macro_use] extern crate custom_derive;
+
+trait TypeName {
+    fn type_name() -> &'static str;
+}
+
+trait ReprType {
+    type Repr;
+}
+
+macro_rules! TypeName {
+    (() $(pub)* enum $name:ident $($tail:tt)*) => { TypeName! { @impl $name } };
+    (() $(pub)* struct $name:ident $($tail:tt)*) => { TypeName! { @impl $name } };
+
+    (@impl $name:ident) => {
+        impl TypeName for $name {
+            fn type_name() -> &'static str { stringify!($name) }
+        }
+    };
+}
+
+macro_rules! TryFrom {
+    (($repr:ty) $(pub)* enum $name:ident $($tail:tt)*) => {
+        impl ReprType for $name {
+            type Repr = $repr;
+        }
+    };
+}
+
+custom_derive! {
+    #[allow(dead_code)]
+    #[repr(u8)]
+    #[derive(Clone, Copy, Debug, TryFrom(u8), TypeName)]
+    enum Foo { A, B }
+}
+
+fn main() {
+    let foo = Foo::B;
+    let v = foo as <Foo as ReprType>::Repr;
+    let msg = format!("{}: {:?} ({:?})", Foo::type_name(), foo, v);
+    assert_eq!(msg, "Foo: B (1)");
+}
+```
+
+First, note that `custom_derive!` passes any arguments on the derivation attribute to the macro.  In the case of attributes *without* any arguments, `()` is passed instead.
+
+Secondly, the macro is passed the entire item, *sans* attributes.  It is the derivation macro's job to parse the item correctly.
+
+Third, each derivation macro is expected to result in zero or more items, not including the item itself.  As a result, it is *not* possible to mutate the item in any way, or attach additional attributes to it.
+
+Finally, `@impl` is merely a trick to pack multiple, different functions into a single macro.  The sequence has no special meaning; it is simply *distinct* from the usual invocation syntax.
 */
+
+#[doc(hidden)]
 #[macro_export]
 macro_rules! custom_derive {
+    /*
+
+    > **Convention**: a capture named `$fixed` is used for any part of a recursive rule that is needed in the terminal case, but is not actually being *used* for the recursive part.  This avoids having to constantly repeat the full capture pattern (and makes changing it easier).
+
+    # Primary Invocation Forms
+
+    These need to catch any valid form of struct or enum.
+
+    */
     (
         $(#[$($attrs:tt)*])*
         enum $($it:tt)*
@@ -36,8 +117,18 @@ macro_rules! custom_derive {
         }
     };
 
-    (@as_item $($i:item)*) => {$($i)*};
+    /*
 
+    # `@split_attrs`
+
+    This is responsible for dividing all attributes on an item into two groups:
+
+    - `#[derive(...)]`
+    - Everything else.
+
+    As part of this, it also explodes `#[derive(A, B(..), C, ...)]` into `A, B(..), C, ...`.  This is to simplify the next stage.
+
+    */
     (
         @split_attrs
         (),
@@ -87,9 +178,16 @@ macro_rules! custom_derive {
     };
 
     /*
-    Built-in derives:
-        Clone, Hash, RustcEncodable, RustcDecodable, PartialEq, Eq, PartialOrd, Ord,
-        Debug, Default, Send, Sync, Copy,
+
+    # `@split_derive_attrs`
+
+    This is responsible for taking the list of derivation attributes and splitting them into "built-in" and "custom" groups.
+
+    The list of built-in derives currently supported is: Clone, Hash, RustcEncodable, RustcDecodable, PartialEq, Eq, PartialOrd, Ord, Debug, Default, Send, Sync, Copy.
+
+    Anything not on that list is considered "custom".
+
+    And yes, as far as I can see, we *have* to have a separate rule for each of those.  What I wouldn't give for an alternation pattern...
     */
 
     (@split_derive_attrs
@@ -241,6 +339,15 @@ macro_rules! custom_derive {
         }
     };
 
+    /*
+
+    ## Custom Derivations
+
+    Now we can handle the custom derivations.  There are two forms we care about: those *with* an argument, and those *without*.
+
+    The *reason* we care is that, in order to simplify the derivation macros, we want to detect the argument-less case and generate an empty pair of parens.
+
+    */
     (@split_derive_attrs
         $fixed:tt,
         ($new_user:ident, $($tail:tt)*), $bi_drvs:tt, ($($user_drvs:tt)*)
@@ -261,6 +368,15 @@ macro_rules! custom_derive {
         }
     };
 
+    /*
+
+    # `@expand_user_drvs`
+
+    Finally, we have a recursive rule for expanding user derivations.  This is basically just using the derivation name as a macro identifier.
+
+    This *has* to be recursive because we need to expand two independent repetition sequences simultaneously, and this causes `macro_rules!` to throw a wobbly.  Don't want that.  So, recursive it is.
+
+    */
     (@expand_user_drvs
         (), ($($it:tt)*)
     ) => {};
@@ -274,4 +390,11 @@ macro_rules! custom_derive {
             ($($tail)*), ($($it)*)
         }
     };
+
+    /*
+
+    # Miscellaneous Rules
+
+    */
+    (@as_item $($i:item)*) => {$($i)*};
 }

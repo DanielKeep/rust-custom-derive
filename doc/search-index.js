6db58f09fb883db3d3d0c861956fb96a45b97404
Daniel Keep <daniel.keep@gmail.com>
2015-08-02 01:42:23 +1000

    Update docs for a62e5a2
    Added metadata, README.md and LICENSE.

---
+++ doc/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['custom_derive'] = {"items":[[0,"","custom_derive","This crate provides a macro that enables the use of custom `derive` attributes.",null,null]],"paths":[]};
+initSearch(searchIndex);

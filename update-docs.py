4d7779b6539c56ad29ee0a2878cbc6e3acaffb5a
Daniel Keep <daniel.keep@gmail.com>
2015-08-16 19:47:45 +1000

    Updated doc generation to use doc package.
    This is so that we can generate docs for *all* packages in one place.

--- update-docs.py
+++ update-docs.py
@@ -10,6 +10,7 @@ import time
 
 USE_ANSI = True if sys.platform != 'win32' else os.environ.get('FORCE_ANSI', '') != ''
 TRACE_UPDATE_DOCS = os.environ.get('TRACE_UPDATE_DOCS', '') != ''
+DOC_PKG_DIR = 'doc-pkg'
 
 def sh(cmd):
     msg_trace('sh(%r)' % cmd)
@@ -92,6 +93,32 @@ def really_rmtree(path):
 
     msg('Warning: failed to remove directory %r' % path)
 
+def gen_doc_bare(tmp1, tmp2):
+    msg("Generating documentation from regular package...")
+    sh('cargo doc')
+    tmp1_target_doc = '%s/target/doc' % tmp1
+    msg_trace('shutil.move(%r, %r)' % (tmp1_target_doc, tmp2))
+    shutil.move(tmp1_target_doc, tmp2)
+
+def gen_doc_pkg(tmp1, tmp2, doc_pkg):
+    import json
+    old_dir = os.getcwdu()
+    try:
+        msg("Generating documentation from doc package...")
+        msg_trace('doc_pkg = %r' % doc_pkg)
+        msg_trace('os.chdir(%r)' % doc_pkg)
+        os.chdir(doc_pkg)
+        manifest_str = sh_eval('cargo read-manifest --manifest-path "%s"' % doc_pkg)
+        manifest = json.loads(manifest_str)
+        for dep in (d['name'] for d in manifest['dependencies']):
+            sh('cargo doc --package %s' % dep)
+        target_doc = os.path.join(doc_pkg, 'target/doc')
+        msg_trace('shutil.move(%r, %r)' % (target_doc, tmp2))
+        shutil.move(target_doc, tmp2)
+    finally:
+        msg_trace('os.chdir(%r)' % old_dir)
+        os.chdir(old_dir)
+
 def main():
     if sh_eval('git symbolic-ref --short HEAD') != u'master': return 0
 
@@ -115,11 +142,10 @@ def main():
         os.chdir(tmp1)
         sh('git checkout -q master')
 
-        msg("Generating documentation...")
-        sh('cargo doc')
-        tmp1_target_doc = '%s/target/doc' % tmp1
-        msg_trace('shutil.move(%r, %r)' % (tmp1_target_doc, tmp2))
-        shutil.move(tmp1_target_doc, tmp2)
+        if not os.path.exists(DOC_PKG_DIR):
+            gen_doc_bare(tmp1, tmp2)
+        else:
+            gen_doc_pkg(tmp1, tmp2, os.path.abspath(DOC_PKG_DIR))
 
         msg('Updating gh-pages...')
         sh('git checkout -q gh-pages')

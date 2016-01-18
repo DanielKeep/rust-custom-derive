81a947db90514bc6a77fb3f20fe9df5ed6aefc42
Daniel Keep <daniel.keep@gmail.com>
2016-01-18 17:01:05 +1100

    Add regression tests and `std-unstable` fixes for #5.
    Closes #5.

--- newtype_derive/src/std_unstable.rs
+++ newtype_derive/src/std_unstable.rs
@@ -2,7 +2,15 @@
 
 #[macro_export]
 macro_rules! NewtypeOne {
-    (() $(pub)* struct $name:ident($(pub)* $_t0:ty);) => {
+    (() $(pub)* struct $name:ident(pub $_t0:ty);) => {
+        impl ::std::num::One for $name {
+            fn one() -> Self {
+                $name(::std::num::One::one())
+            }
+        }
+    };
+
+    (() $(pub)* struct $name:ident($_t0:ty);) => {
         impl ::std::num::One for $name {
             fn one() -> Self {
                 $name(::std::num::One::one())
@@ -13,7 +21,15 @@ macro_rules! NewtypeOne {
 
 #[macro_export]
 macro_rules! NewtypeZero {
-    (() $(pub)* struct $name:ident($(pub)* $_t0:ty);) => {
+    (() $(pub)* struct $name:ident(pub $_t0:ty);) => {
+        impl ::std::num::Zero for $name {
+            fn zero() -> Self {
+                $name(::std::num::Zero::zero())
+            }
+        }
+    };
+
+    (() $(pub)* struct $name:ident($_t0:ty);) => {
         impl ::std::num::Zero for $name {
             fn zero() -> Self {
                 $name(::std::num::Zero::zero())

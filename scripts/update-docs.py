23370e2a48487ba84299cc6050016bf18dd20f56
Daniel Keep <daniel.keep@gmail.com>
2016-04-22 15:49:14 +1000

    Updated update-docs.py and moved into scripts directory.

--- scripts/update-docs.py
+++ scripts/update-docs.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # coding: utf-8
 
-# Copyright ⓒ 2015, 2016 Daniel Keep.
+# Copyright ⓒ 2016 Daniel Keep.
 #
 # Licensed under the MIT license (see LICENSE or <http://opensource.org
 # /licenses/MIT>) or the Apache License, Version 2.0 (see LICENSE of
@@ -17,9 +17,15 @@ import sys
 import tempfile
 import time
 
+DOC_ARGS = '--no-deps'
+DOC_FEATURES = ""
+DOC_TARGET_BRANCH = 'gh-pages'
+DOC_PKG_DIR = 'doc-pkg'
+TEMP_CHECKOUT_PREFIX = 'gh-pages-checkout-'
+TEMP_OUTPUT_PREFIX = 'gh-pages-generated-'
+
 USE_ANSI = True if sys.platform != 'win32' else os.environ.get('FORCE_ANSI', '') != ''
 TRACE_UPDATE_DOCS = os.environ.get('TRACE_UPDATE_DOCS', '') != ''
-DOC_PKG_DIR = 'doc-pkg'
 
 def sh(cmd):
     msg_trace('sh(%r)' % cmd)
@@ -29,11 +35,13 @@ def sh(cmd):
         msg_trace('FAILED!')
         raise
 
-def sh_eval(cmd, codec='utf-8'):
+def sh_eval(cmd, codec='utf-8', dont_strip=False):
     msg_trace('sh_eval(%r)' % cmd)
     result = None
     try:
-        result = subprocess.check_output(cmd, shell=True).decode(codec).strip()
+        result = subprocess.check_output(cmd, shell=True).decode(codec)
+        if not dont_strip:
+            result = result.strip()
     except:
         msg_trace('FAILED!')
         raise
@@ -102,20 +110,53 @@ def really_rmtree(path):
 
     msg('Warning: failed to remove directory %r' % path)
 
+def init_doc_branch():
+    msg("Initialising %s branch" % DOC_TARGET_BRANCH)
+
+    dir = os.getcwd()
+    msg_trace('dir = %r' % dir)
+
+    tmp = tempfile.mkdtemp(prefix=TEMP_CHECKOUT_PREFIX)
+    msg_trace('tmp = %r' % tmp)
+
+    try:
+        msg("Cloning into a temporary directory...")
+        sh('git init -q "%s"' % tmp)
+        msg_trace('os.chdir(%r)' % tmp)
+        os.chdir(tmp)
+        sh('git checkout -q --orphan "%s"' % DOC_TARGET_BRANCH)
+        sh('git commit -qm "Initial commit." --allow-empty')
+        sh('git remote add origin "%s"' % dir)
+        sh('git push -q origin gh-pages')
+
+    finally:
+        msg('Cleaning up...')
+        msg_trace('os.chdir(%r)' % dir)
+        os.chdir(dir)
+        msg_trace('shutil.rmtree(%r)' % tmp)
+        really_rmtree(tmp)
+
+    msg('%s is ready.  Continuing.' % DOC_TARGET_BRANCH)
+
 def gen_doc_bare(tmp1, tmp2):
-    msg("Generating documentation from regular package...")
-    sh('cargo doc')
+    msg("Generating documentation...")
+    args = '%s --features="%s"' % (DOC_ARGS, DOC_FEATURES)
+    sh('cargo doc %s' % args)
     tmp1_target_doc = '%s/target/doc' % tmp1
     msg_trace('shutil.move(%r, %r)' % (tmp1_target_doc, tmp2))
     shutil.move(tmp1_target_doc, tmp2)
 
 def gen_doc_pkg(tmp1, tmp2, doc_pkg):
+    if DOC_FEATURES != "":
+        msg("Error: doc packages don't support features.")
+        sys.exit(1)
+
     import json
-    old_dir = os.getcwdu()
+    old_dir = os.getcwd()
+    msg("Generating documentation from doc package...")
+    msg_trace('doc_pkg = %r' % doc_pkg)
+    msg_trace('os.chdir(%r)' % doc_pkg)
     try:
-        msg("Generating documentation from doc package...")
-        msg_trace('doc_pkg = %r' % doc_pkg)
-        msg_trace('os.chdir(%r)' % doc_pkg)
         os.chdir(doc_pkg)
         manifest_str = sh_eval('cargo read-manifest --manifest-path "%s"'
             % os.path.join(doc_pkg, 'Cargo.toml'))
@@ -130,35 +171,43 @@ def gen_doc_pkg(tmp1, tmp2, doc_pkg):
         os.chdir(old_dir)
 
 def main():
-    if sh_eval('git symbolic-ref --short HEAD') != u'master': return 0
+    if sh_eval('git symbolic-ref --short HEAD') != u'master':
+        msg('Not on master; doing nothing.')
+        return 0
+
+    # Sanity check: does the doc branch exist at all?
+    branches = {b[2:].strip() for b in sh_eval('git branch', dont_strip=True).splitlines()}
+    msg_trace('branches = %r' % branches)
+    if DOC_TARGET_BRANCH not in branches:
+        init_doc_branch()
 
     last_rev = sh_eval('git rev-parse HEAD')
     last_msg = sh_eval('git log -1 --pretty=%B')
     msg_trace('last_rev = %r' % last_rev)
     msg_trace('last_msg = %r' % last_msg)
 
-    dir = os.getcwdu()
+    dir = os.getcwd()
     msg_trace('dir = %r' % dir)
 
-    tmp1 = tempfile.mkdtemp(prefix='gh-pages-post-commit-checkout-')
-    tmp2 = tempfile.mkdtemp(prefix='gh-pages-post-commit-temp-')
+    tmp1 = tempfile.mkdtemp(prefix=TEMP_CHECKOUT_PREFIX)
+    tmp2 = tempfile.mkdtemp(prefix=TEMP_OUTPUT_PREFIX)
     msg_trace('tmp1 = %r' % tmp1)
     msg_trace('tmp2 = %r' % tmp2)
 
     try:
         msg("Cloning into a temporary directory...")
-        sh('git clone -qb gh-pages "%s" "%s"' % (dir, tmp1))
+        sh('git clone -qb "%s" "%s" "%s"' % (DOC_TARGET_BRANCH, dir, tmp1))
         msg_trace('os.chdir(%r)' % tmp1)
         os.chdir(tmp1)
         sh('git checkout -q master')
 
-        if not os.path.exists(DOC_PKG_DIR):
-            gen_doc_bare(tmp1, tmp2)
-        else:
+        if DOC_PKG_DIR is not None:
             gen_doc_pkg(tmp1, tmp2, os.path.abspath(DOC_PKG_DIR))
+        else:
+            gen_doc_bare(tmp1, tmp2)
 
-        msg('Updating gh-pages...')
-        sh('git checkout -q gh-pages')
+        msg('Updating %s...' % DOC_TARGET_BRANCH)
+        sh('git checkout -q "%s"' % DOC_TARGET_BRANCH)
         sh('git clean -dfq')
         tmp2_doc = '%s/doc' % tmp2
 
@@ -167,9 +216,9 @@ def main():
 
         msg('Committing changes...')
         sh('git add .')
-        sh('git commit -m "Update docs for %s" -m "%s"' % (last_rev[:7], last_msg))
+        sh('git commit --amend -m "Update docs for %s" -m "%s"' % (last_rev[:7], last_msg))
 
-        sh('git push -qu origin gh-pages')
+        sh('git push -fqu origin "%s"' % DOC_TARGET_BRANCH)
 
     finally:
         msg('Cleaning up...')
@@ -180,7 +229,10 @@ def main():
         msg_trace('shutil.rmtree(%r)' % tmp1)
         really_rmtree(tmp1)
 
-    msg('Done.  Use `git push origin gh-pages` to update live documentation.')
+    msg('Publishing...')
+    sh('git push -f origin "%s"' % DOC_TARGET_BRANCH)
+
+    msg('Done.')
 
 
 if __name__ == '__main__':

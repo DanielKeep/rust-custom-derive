2688335f576ff7d12c71cc65a823dcd7589baa48
Daniel Keep <daniel.keep@gmail.com>
2017-09-22 14:48:02 +1000

    Added EnumTag! to enum_derive.

--- enum_derive/src/lib.rs
+++ enum_derive/src/lib.rs
@@ -144,6 +144,56 @@ macro_rules! enum_derive_util {
         }
     };
 
+    (
+        @collect_variant_names ($callback:ident { $($args:tt)* }),
+        ($(,)*) -> ($($out:tt)*)
+    ) => {
+        enum_derive_util! {
+            @as_item
+            $callback!{ $($args)* ($($out)*) }
+        }
+    };
+
+    (
+        @collect_variant_names $fixed:tt,
+        (#[$_attr:meta] $($tail:tt)*) -> ($($out:tt)*)
+    ) => {
+        enum_derive_util! {
+            @collect_variant_names $fixed,
+            ($($tail)*) -> ($($out)*)
+        }
+    };
+
+    (
+        @collect_variant_names $fixed:tt,
+        ($var_name:ident, $($tail:tt)*) -> ($($out:tt)*)
+    ) => {
+        enum_derive_util! {
+            @collect_variant_names $fixed,
+            ($($tail)*) -> ($($out)* $var_name,)
+        }
+    };
+
+    (
+        @collect_variant_names $fixed:tt,
+        ($var_name:ident($($_var_tuple:tt)*), $($tail:tt)*) -> ($($out:tt)*)
+    ) => {
+        enum_derive_util! {
+            @collect_variant_names $fixed,
+            ($($tail)*) -> ($($out)* $var_name,)
+        }
+    };
+
+    (
+        @collect_variant_names $fixed:tt,
+        ($var_name:ident { $($_var_struct:tt)* }, $($tail:tt)*) -> ($($out:tt)*)
+    ) => {
+        enum_derive_util! {
+            @collect_variant_names $fixed,
+            ($($tail)*) -> ($($out)* $var_name,)
+        }
+    };
+
     (@error item $msg:tt) => {
         const $msg: () = ();
     };
@@ -1071,6 +1121,84 @@ macro_rules! EnumInnerAsTrait {
         enum_derive_util! {
             @collect_unary_variants
             (EnumInnerAsTrait { @expand $arg, $name, }),
+            ($($body)*,) -> ()
+        }
+    };
+}
+
+/**
+```ignore
+macro_attr! {
+    #[derive(EnumTag!($fn_name -> #[derive(Debug)] $kind_ty))]
+    #[derive(EnumTag!(pub $fn_name -> $kind_ty))]
+    enum $name { ... }
+}
+```
+
+Derives a method that returns a "tag" enumeration value.  The tag enumeration consists of the same variants as `$name`, except with all payloads removed.  Attributes written prior to `$kind_ty` are attached to the generated tag enumeration.
+
+Note that macro attributes are not supported on the tag enumeration.
+
+```rust
+# #[macro_use] extern crate macro_attr;
+# #[macro_use] extern crate enum_derive;
+macro_attr! {
+    #[derive(EnumTag!(pub tag -> #[derive(Debug, PartialEq)] ComestiblesTag))]
+    enum Comestibles { Egg(i32), Sausage(i32), Bacon(i32), Spam(i32) }
+}
+
+# fn main() {
+assert_eq!(Comestibles::Sausage(2).tag(), ComestiblesTag::Sausage);
+# }
+```
+*/
+#[macro_export]
+macro_rules! EnumTag {
+    (
+        @expand ($($vis:tt)*), $name:ident, $fn_name:ident,
+        {$($attrs:tt)*}, $tag_ty:ident,
+        ($($var_names:ident,)*)
+    ) => {
+        enum_derive_util! {
+            @as_item
+            impl $name {
+                #[inline]
+                $($vis)* fn $fn_name(&self) -> $tag_ty {
+                    match *self {
+                        $(
+                            $name::$var_names(..) => $tag_ty::$var_names,
+                        )*
+                    }
+                }
+            }
+
+            $($attrs)*
+            $($vis)* enum $tag_ty {
+                $(
+                    $var_names,
+                )*
+            }
+        }
+    };
+
+    (
+        (pub $fn_name:ident -> $(#[$($attrs:tt)*])* $tag_ty:ident)
+        $(pub)* enum $name:ident { $($body:tt)* }
+    ) => {
+        enum_derive_util! {
+            @collect_variant_names
+            (EnumTag { @expand (pub), $name, $fn_name, {$(#[$($attrs)*])*}, $tag_ty, }),
+            ($($body)*,) -> ()
+        }
+    };
+
+    (
+        ($fn_name:ident -> $tag_ty:ident)
+        $(pub)* enum $name:ident { $($body:tt)* }
+    ) => {
+        enum_derive_util! {
+            @collect_variant_names
+            (EnumTag { @expand (), $name, $fn_name, {}, $tag_ty, }),
             ($($body)*,) -> ()
         }
     };
